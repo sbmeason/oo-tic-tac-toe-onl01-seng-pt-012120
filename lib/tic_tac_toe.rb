7229fcaacdd92bad83ffb709087b164a0ebafa7b
Samantha Meason <sbmeason@gmail.com>
2020-02-16 18:31:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,8 +28,8 @@ def input_to_index(user_input)
     user_input.to_i - 1
   end
 
-def move(board, index, token)
-    board[index] = token
+def move(index, token)
+    @board[index] = token
   end
   
   
