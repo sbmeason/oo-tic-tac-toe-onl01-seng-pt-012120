5c7f994930a5e5f14fc00786dfad0031b79fde7f
Samantha Meason <sbmeason@gmail.com>
2020-02-16 18:29:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,13 +16,13 @@ WIN_COMBINATIONS = [
   ]
 
 
-def display_board(board)
+ def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-end
+  end
   
 def input_to_index(input)
     input.to_i - 1
