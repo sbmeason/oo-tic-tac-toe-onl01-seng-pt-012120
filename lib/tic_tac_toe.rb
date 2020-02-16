76294b01522ec3d90abba5278925193bc4226af8
Samantha Meason <sbmeason@gmail.com>
2020-02-16 18:28:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,8 +1,8 @@
 class TicTacToe
-
-def initlialize(board = nil)
-  @board = board || Array.new(9, " ")
-end
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+end 
 
 WIN_COMBINATIONS = [
   [0,1,2],
